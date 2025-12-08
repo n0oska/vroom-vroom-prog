8ed520ac63b27c0a67715a2cb2717b80d23ed70a
n0oska <noah.touchet.2005@gmail.com>
2025-12-08 16:43:29 +0100

    carcarcvarcarcarcarcarSUPERCAR

--- Source/vroom_vroom/ACarVehicle.cpp
+++ Source/vroom_vroom/ACarVehicle.cpp
@@ -1,51 +1,74 @@
-#include "Kismet/GameplayStatics.h"
-
 #include "ACarVehicle.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
 
 AACarVehicle::AACarVehicle()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
 	CarMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CarMesh"));
 	SetRootComponent(CarMesh);
 	CarMesh->SetSimulatePhysics(true);
 
+	MaxAcceleration = 70000.f;
+	AccelerationSpeed = 200000.f;
+	MaxSteeringTorque = 800000.f;
+	ThrottleInput = 0.f;
+	SteeringInput = 0.f;
+	CurrentSpeed = 0.f;
 }
 
 void AACarVehicle::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(IMC_Vroom, 0);
+		}
+	}
 }
 
-void AACarVehicle::MoveForward(float Value)
+void AACarVehicle::Move2D(const FInputActionValue& Value)
 {
-	ThrottleInput = Value;
-	
-}
+	FVector2D Input = Value.Get<FVector2D>();
 
-void AACarVehicle::MoveRight(float Value)
-{
-	SteeringInput = Value;
+	TargetThrottle = Input.Y;
+	TargetSteering = Input.X;
 }
-
 
 void AACarVehicle::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
-	FVector Force = CarMesh->GetForwardVector()*ThrottleInput*70000.0f;
-	CarMesh->AddForce(Force);
-	
-	FVector	Torque = FVector(0, 0, SteeringInput*8000.f);
-	CarMesh->AddTorqueInRadians(Torque);
 
+	CurrentSpeed = FMath::FInterpTo(CurrentSpeed, TargetThrottle, DeltaTime, AccelerationSpeed);
+
+	FVector Force = CarMesh->GetForwardVector() * CurrentSpeed * MaxAcceleration;
+	CarMesh->AddForce(Force);
+
+	if (FMath::Abs(CurrentSpeed) < 0.05f)
+	{
+		SteeringInput = TargetSteering;
+	}
+	else
+	{
+		SteeringInput = 0.f;
+	}
+
+	FVector Torque(0, 0, SteeringInput * MaxSteeringTorque);
+	CarMesh->AddTorqueInRadians(Torque);
 }
 
 void AACarVehicle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AACarVehicle::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AACarVehicle::MoveRight);
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInput->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AACarVehicle::Move2D);
+	}
 }
-
--- Source/vroom_vroom/ACarVehicle.h
+++ Source/vroom_vroom/ACarVehicle.h
@@ -1,13 +1,15 @@
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "MyGameMode.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 
 #include "ACarVehicle.generated.h"
-UCLASS()
 
+UCLASS()
 class VROOM_VROOM_API AACarVehicle : public APawn
 {
 	GENERATED_BODY()
@@ -15,25 +17,37 @@ class VROOM_VROOM_API AACarVehicle : public APawn
 public:
 	AACarVehicle();
 	virtual void Tick(float DeltaTime) override;
-
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	virtual void BeginPlay() override;
-	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputMappingContext* IMC_Vroom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* IA_Move;
+
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CarMesh;
-	
 
-	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	AACarVehicle* OtherCar;
-	
+
 	float ThrottleInput;
 	float SteeringInput;
-	
-	void MoveForward(float Value);
-	void MoveRight(float Value);
-public:	
+	float TargetThrottle;
+	float TargetSteering;
+	float CurrentSpeed;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Car|Physics")
+	float MaxAcceleration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Car|Physics")
+	float AccelerationSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Car|Physics")
+	float MaxSteeringTorque;
+
+	void Move2D(const FInputActionValue& Value);
 };
